03a4b735d8c3e023538ad0bc18fc90e830063dc3
lw-ajc <apart_montesson@outlook.fr>
2023-04-12 15:14:01 +0200

    transfert du projet Spring Boot préconfiguré

---
+++ src/main/java/ajc/formation/spring/bibliotheque/Application.java
@@ -0,0 +1,13 @@
+package ajc.formation.spring.bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ajc/formation/spring/bibliotheque/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ajc.formation.spring.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
