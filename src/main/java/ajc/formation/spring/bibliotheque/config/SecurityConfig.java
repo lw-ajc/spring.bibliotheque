2dc18fedb8413986d8711f2b0c6206165312cdbd
lw-ajc <apart_montesson@outlook.com>
2023-04-21 12:01:50 +0200

    Ajout de la sécurité Spring sans la classe Utilisateur

---
+++ src/main/java/ajc/formation/spring/bibliotheque/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package ajc.formation.spring.bibliotheque.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		//@formatter:off
+		return http.antMatcher("/api/**")
+				.csrf().disable()
+//				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//				.and()
+				.authorizeRequests()
+					.antMatchers(HttpMethod.OPTIONS).permitAll()
+//					.antMatchers(HttpMethod.GET, "/api/compte/login/check/**").permitAll()
+//					.antMatchers(HttpMethod.POST,"/api/client/inscription").anonymous()
+//					.antMatchers(HttpMethod.POST, "/api/commande").hasAnyRole("USER")
+//					.antMatchers(HttpMethod.GET).authenticated()
+					.anyRequest().hasAnyRole("ADMIN")
+				.and()
+				.httpBasic()
+				.and()
+				.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/main/java/ajc/formation/spring/bibliotheque/entities/Role.java
@@ -0,0 +1,6 @@
+package ajc.formation.spring.bibliotheque.entities;
+
+
+public enum Role {
+	ROLE_ADMIN,ROLE_ADHERENT;
+}
